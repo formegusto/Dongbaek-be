934ac8520aa990766f5d75f50bee2eacde812d1b
formegusto <formegusto@gmail.com>
2022-06-11 19:50:45 +0900

    Polaroid는 제목이 없어도 되는 것이 맞는거같죠\?

--- src/models/dongbaek/index.ts
+++ src/models/dongbaek/index.ts
@@ -3,7 +3,7 @@ import { Dongbaek } from "./types";
 
 const DongbaekSchema = new Schema<Dongbaek>(
   {
-    title: { type: String, required: true },
+    title: { type: String },
     image: { type: String, required: true },
     createdAt: { type: String, required: true },
     _userId: { type: String, required: true },
